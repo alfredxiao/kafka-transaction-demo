1001d4a6b5213eb77bd46b911dff29ee6bcfbc36
Alfred Xiao <axiao@limepoint.com>
2021-12-17 22:47:58 +1100

    init

---
+++ src/main/java/xiaoyf/demo/kafka/transaction/Constants.java
@@ -0,0 +1,8 @@
+package xiaoyf.demo.kafka.transaction;
+
+public interface Constants {
+    String BOOTSTRAP_SERVERS = "localhost:9092";
+    String GROUPD_ID_1 = "group1";
+    String LOWERCASE_TOPIC = "lowercase";
+    String UPPERCASE_TOPIC = "uppercase";
+}
