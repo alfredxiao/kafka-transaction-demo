c5af94607c7cc893a874c2b6853a1201a832ccae
Alfred Xiao <alfred.xiao@qantashotels.com>
2022-08-26 18:27:26 +1000

    add zombie fencing demo

--- src/main/java/xiaoyf/demo/kafka/transaction/Constants.java
+++ src/main/java/xiaoyf/demo/kafka/transaction/Constants.java
@@ -5,4 +5,7 @@ public interface Constants {
     String GROUPD_ID_1 = "group1";
     String LOWERCASE_TOPIC = "lowercase";
     String UPPERCASE_TOPIC = "uppercase";
+    String SINGLE_TRANSACTIONAL_PRODUCER_TOPIC = "single-transactional-producer";
+    String ANOTHER_TOPIC = "another";
+    String ONE_TX_AND_ONE_NON_TX_PRODUCER_TOPIC = "one-tx-and-one-non-tx-producer";
 }
